aee6df095d3a4f1bc81ea793236dca677d3b72fd
amey.haldankar07@gmail.com <amey.haldankar07@gmail.com@3527beb6-7c4c-11e0-7c57-24903e89fb06>
2014-03-25 14:29:35 +0000

    #beta version #static #sms can be send #alarm is set #sms is being send when alarm is triggered. #date 25/03/2014

---
+++ SMSScheduler/src/com/android/smsscheduler/MainActivity.java
@@ -0,0 +1,93 @@
+package com.android.smsscheduler;
+
+import java.util.GregorianCalendar;
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends Activity
+{
+	Button sendBtn ;
+	EditText smsEdit;
+	String smsStr, phoneNo;
+	String TAG = "MainActivity" ;
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		sendBtn = (Button)findViewById(R.id.sendBtn);
+		smsEdit = (EditText)findViewById(R.id.smsEdit);
+		smsStr = "";
+		phoneNo = "8800839680";
+
+		sendBtn.setOnClickListener(new OnClickListener()
+		{
+
+			@Override
+			public void onClick(View v)
+			{
+				//sendSMS();
+				scheduleAlarm();
+			}
+		});
+
+	}
+
+	public void sendSMS()
+	{
+		smsStr = smsEdit.getText().toString();
+
+		if(smsStr != null)
+		{
+			try
+			{
+				SmsManager smsMgr = SmsManager.getDefault() ;
+				smsMgr.sendTextMessage(phoneNo, null, smsStr, null, null);
+				Toast.makeText(getApplicationContext(), "SMS Sent!",
+						Toast.LENGTH_LONG).show();
+			}
+			catch(Exception e)
+			{
+				Toast.makeText(getApplicationContext(),
+						"SMS faild, please try again later!",
+						Toast.LENGTH_LONG).show();
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void scheduleAlarm()
+	{
+		smsStr = smsEdit.getText().toString();
+		
+		Long time = new GregorianCalendar().getTimeInMillis()+60*1000;
+		
+		Intent intent = new Intent(MainActivity.this, AlaramReceiver.class);
+		
+		Bundle b = new Bundle() ;
+		b.putString("sms_text", smsStr);
+		intent.putExtras(b);
+		
+		AlarmManager alarmMgr = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+		
+		alarmMgr.set(AlarmManager.RTC_WAKEUP,time, PendingIntent.getBroadcast(this,1,  intent,
+				PendingIntent.FLAG_UPDATE_CURRENT));
+		Log.i(TAG, "Alarm Scheduled :"+time);
+		
+		Toast.makeText(getApplicationContext(), "Alarm Scheduled ",
+				Toast.LENGTH_LONG).show();
+	}
+}
