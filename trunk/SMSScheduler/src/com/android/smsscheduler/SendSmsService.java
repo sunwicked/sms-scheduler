8fc4b576e7053e5fcdbcbc8d055b2106ac6dde67
sunnykraditya@gmail.com <sunnykraditya@gmail.com@3527beb6-7c4c-11e0-7c57-24903e89fb06>
2014-03-26 18:39:27 +0000

    #skeleton files

--- trunk/SMSScheduler/src/com/android/smsscheduler/SendSmsService.java
+++ trunk/SMSScheduler/src/com/android/smsscheduler/SendSmsService.java
@@ -2,8 +2,15 @@ package com.android.smsscheduler;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.widget.Toast;
 
-public class SendSmsService extends IntentService{
+public class SendSmsService extends IntentService {
+
+	String phoneNo = "8800839680";
+	String smsStr = "This is msg from application which is  scheduled ";
+	String TAG = "AlaramReceiver";
 
 	public SendSmsService(String name) {
 		super(name);
@@ -12,8 +19,16 @@ public class SendSmsService extends IntentService{
 
 	@Override
 	protected void onHandleIntent(Intent intent) {
-		// TODO Auto-generated method stub
-		
+
+		try {
+			smsStr = intent.getExtras().getString("sms_text");
+			Log.i(TAG, "" + smsStr);
+			SmsManager smsMgr = SmsManager.getDefault();
+			smsMgr.sendTextMessage(phoneNo, null, smsStr, null, null);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
