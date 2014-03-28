071d7de2c72254a17a065d48fb13eaa3d4cac6ac
amey.haldankar07@gmail.com <amey.haldankar07@gmail.com@3527beb6-7c4c-11e0-7c57-24903e89fb06>
2014-03-28 05:22:42 +0000

    -2# -timepicker and datepicker added

--- SMSScheduler/src/com/android/smsscheduler/MainActivity.java
+++ SMSScheduler/src/com/android/smsscheduler/MainActivity.java
@@ -2,21 +2,29 @@ package com.android.smsscheduler;
 
 import java.util.GregorianCalendar;
 
+import com.android.smsfragments.DatePickerFragment;
+import com.android.smsfragments.TimePickerFragment;
+
 import android.app.Activity;
 import android.app.AlarmManager;
+import android.app.DialogFragment;
 import android.app.PendingIntent;
+import android.app.TimePickerDialog;
+import android.app.TimePickerDialog.OnTimeSetListener;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
-public class MainActivity extends Activity
+public class MainActivity extends FragmentActivity
 {
 	Button sendBtn ;
 	EditText smsEdit;
@@ -39,35 +47,28 @@ public class MainActivity extends Activity
 			@Override
 			public void onClick(View v)
 			{
-				//sendSMS();
 				scheduleAlarm();
 			}
 		});
 
+		
+		
 	}
 
-	public void sendSMS()
+	public void showTimePickerDialog(View view)
 	{
-		smsStr = smsEdit.getText().toString();
-
-		if(smsStr != null)
-		{
-			try
-			{
-				SmsManager smsMgr = SmsManager.getDefault() ;
-				smsMgr.sendTextMessage(phoneNo, null, smsStr, null, null);
-				Toast.makeText(getApplicationContext(), "SMS Sent!",
-						Toast.LENGTH_LONG).show();
-			}
-			catch(Exception e)
-			{
-				Toast.makeText(getApplicationContext(),
-						"SMS faild, please try again later!",
-						Toast.LENGTH_LONG).show();
-				e.printStackTrace();
-			}
-		}
+		android.support.v4.app.DialogFragment timeFragment = new TimePickerFragment();
+		timeFragment.show(getSupportFragmentManager(), "time_fragment");
+		
 	}
+	
+	public void showDatePickerDialog(View view)
+	{
+		android.support.v4.app.DialogFragment dateFragment = new DatePickerFragment();
+		dateFragment.show(getSupportFragmentManager(), "date_fragment");
+	}
+	
+	
 
 	public void scheduleAlarm()
 	{
