245424e4020d4a1e7cc95ea8913fbb251bd06bb1
sunnykraditya@gmail.com <sunnykraditya@gmail.com@3527beb6-7c4c-11e0-7c57-24903e89fb06>
2014-03-30 09:19:35 +0000

    #new layout and activity

---
+++ trunk/SMSScheduler/src/com/android/smsscheduler/AddNewSmsReminder.java
@@ -0,0 +1,17 @@
+package com.android.smsscheduler;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddNewSmsReminder extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.add_new_layout);
+	}
+	
+	
+
+}
