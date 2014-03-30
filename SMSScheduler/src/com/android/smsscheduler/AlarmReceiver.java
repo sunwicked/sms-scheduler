fdb247d39ea6b955388b32b225ea2249cfa08fa3
amey.haldankar07@gmail.com <amey.haldankar07@gmail.com@3527beb6-7c4c-11e0-7c57-24903e89fb06>
2014-03-30 06:39:20 +0000

    -3# -picking contact from contact provider added

--- SMSScheduler/src/com/android/smsscheduler/AlarmReceiver.java
+++ SMSScheduler/src/com/android/smsscheduler/AlarmReceiver.java
@@ -3,16 +3,33 @@ package com.android.smsscheduler;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 
-public class AlarmReceiver extends BroadcastReceiver {
+public class AlarmReceiver extends BroadcastReceiver 
+{
 
 	@Override
-	public void onReceive(Context context, Intent intent) {
+	public void onReceive(Context context, Intent intent) 
+	{
+		try
+		{
+			String smsText = intent.getExtras().getString(Constants.SMS_STR_KEY);
+			String phoneNo = intent.getExtras().getString(Constants.PHONE_NO_KEY);
 
-		Intent msgIntent = new Intent(context, SendSmsService.class);
-		msgIntent
-				.putExtra("sms_text", intent.getExtras().getString("sms_text"));
-		context.startService(msgIntent);
+			Intent msgIntent = new Intent(context, SendSmsService.class);
+			Bundle b = new Bundle();
+			b.putString(Constants.SMS_STR_KEY, smsText);
+			b.putString(Constants.PHONE_NO_KEY, phoneNo);
+			msgIntent.putExtras(b);
+
+			context.startService(msgIntent);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			Log.i("Receiver", "Exception in receiver.");
+		}
 	}
 
 }
